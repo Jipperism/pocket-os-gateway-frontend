5d5fa9ca5c7113090d0f1022a1a8f6b05d78b9d0
jipstavenuiter <jipstavenuiter@gmail.com>
2025-10-08 16:38:53 +0200

    chore: downgrade to nitro v2 for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro-nightly/vite'
+import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
 
 import { wrapVinxiConfigWithSentry } from '@sentry/tanstackstart-react'
 
@@ -17,7 +17,10 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart(),
-    nitro(),
+    nitroV2Plugin(/* 
+      // nitro config goes here, e.g.
+      { preset: 'node-server' }
+    */),
     viteReact(),
   ],
 })
